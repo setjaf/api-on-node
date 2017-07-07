101676b731e73c77c45609fc88a6895bc6957fd5
setjaf <setjafet@gmail.com>
2017-07-07 10:27:15 -0500

    Mejoras a prueba y creacionde funtions

---
+++ myapp/public/js/funtions.js
@@ -0,0 +1,17 @@
+
+function IncPrecio(	ev) {
+		
+}
+
+
+
+
+$(document).ready(function() {
+	
+    $('select').material_select();
+  	
+
+	$("[name='group']").change(function(event) {
+			console.log(event)
+	});
+});
